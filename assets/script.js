736c45830f48925fb25532e6ddfcb211e62255dc
Cienna97 <cmortenson97@gmail.com>
2021-11-29 14:16:24 -0800

    add containers for time blocks

--- assets/script.js
+++ assets/script.js
@@ -11,18 +11,18 @@ $(document).ready(function(){
 })
 
 
-$("#hour6 .description").val(localStorage.getItem("hour6"));
-$("#hour7 .description").val(localStorage.getItem("hour7"));
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
-$("#hour18 .description").val(localStorage.getItem("hour18"));
-$("#hour19 .description").val(localStorage.getItem("hour19"));
-$("#hour20 .description").val(localStorage.getItem("hour20"));
+$("#6am .description").val(localStorage.getItem("hour6"));
+$("#7am .description").val(localStorage.getItem("hour7"));
+$("#8am .description").val(localStorage.getItem("hour8"));
+$("#9am .description").val(localStorage.getItem("hour9"));
+$("#10am .description").val(localStorage.getItem("hour10"));
+$("#11am .description").val(localStorage.getItem("hour11"));
+$("#12pm .description").val(localStorage.getItem("hour12"));
+$("#13pm .description").val(localStorage.getItem("hour13"));
+$("#14pm .description").val(localStorage.getItem("hour14"));
+$("#15pm .description").val(localStorage.getItem("hour15"));
+$("#16pm .description").val(localStorage.getItem("hour16"));
+$("#17pm.description").val(localStorage.getItem("hour17"));
+$("#18pm .description").val(localStorage.getItem("hour18"));
+$("#19pm .description").val(localStorage.getItem("hour19"));
+$("#20pm .description").val(localStorage.getItem("hour20"));
